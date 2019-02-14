10622182ad19e5a1f0c08fc29e5559b1c03e1cc3
kurt miller <kmiller9393@gmail.com>
2019-02-14 16:00:38 -0700

    Add App component, update styling

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Intro from '../Intro/Intro';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Intro />
+      </div>
+    );
+  }
+}
