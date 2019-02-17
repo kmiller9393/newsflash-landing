a2385f776656771b0fd03455f10f6e00a34ca7de
kurt miller <kmiller9393@gmail.com>
2019-02-16 19:42:30 -0700

    Add handleScroll functionality to App.js to pass down to child components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import Intro from '../Intro/Intro';
 import Features from '../Features/Features';
 import Contact from '../Contact/Contact';
+import { configureAnchors } from 'react-scrollable-anchor';
 import './App.css';
 
 export default class App extends Component {
+  handleScroll = () => {
+    configureAnchors({ scrollDuration: 350 });
+  };
+
   render() {
     return (
       <div className="App">
-        <Intro />
-        <Features />
-        <Contact />
+        <Intro handleScroll={this.handleScroll} />
+        <Features handleScroll={this.handleScroll} />
+        <Contact handleScroll={this.handleScroll} />
       </div>
     );
   }
