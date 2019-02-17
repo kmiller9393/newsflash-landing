21d8c5b634066eceef16d8c69fdfda96e3e2f0fb
kurt miller <kmiller9393@gmail.com>
2019-02-16 19:43:48 -0700

    Wrap component in ScrollableAnchor component

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import ScrollableAnchor from 'react-scrollable-anchor';
 import logo from '../../assets/images/newsflash-logo-light.png';
-import downArrow from '../../assets/images/down-arrow.svg';
-import { configureAnchors } from 'react-scrollable-anchor';
+import downArrow from '../../assets/images/lightDown.svg';
 import './Intro.css';
 
 export default class Intro extends Component {
@@ -9,61 +9,59 @@ export default class Intro extends Component {
     window.open('https://newsflash-front.herokuapp.com/sign-up', '_blank');
   };
 
-  handleScroll = () => {
-    configureAnchors({ scrollDuration: 350 });
-  };
-
   render() {
     return (
-      <section className="home-info-container">
-        <header className="home-header">
-          <div>
-            <img className="header-logo" src={logo} alt="logo" />
-          </div>
-          <div className="header-links-container">
+      <ScrollableAnchor id="home">
+        <section className="home-info-container">
+          <header className="home-header">
+            <div>
+              <img className="header-logo" src={logo} alt="logo" />
+            </div>
+            <div className="header-links-container">
+              <a
+                className="header-link"
+                href="#about"
+                onClick={this.props.handleScroll}
+              >
+                About
+              </a>
+              <a
+                className="header-link"
+                href="https://newsflash-front.herokuapp.com/sign-up"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Sign Up
+              </a>
+              <a className="header-link" href="#contact">
+                Contact
+              </a>
+            </div>
+          </header>
+          <article className="introduction-container">
+            <h2 className="introduction-header">
+              Find the news that matters to you.
+            </h2>
+            <p className="introduction-text">
+              Easily share anything across the web and connect with those who
+              share your passions.
+            </p>
+            <button className="introduction-button" onClick={this.handleClick}>
+              Create your free account
+            </button>
+          </article>
+          <footer className="home-intro-footer">
             <a
-              className="header-link"
-              href="#about"
+              className="more-info-container"
               onClick={this.handleScroll}
+              href="#about"
             >
-              About
+              <p className="arrow-text">More Info</p>
+              <img className="info-image" src={downArrow} alt="more info" />
             </a>
-            <a
-              className="header-link"
-              href="https://newsflash-front.herokuapp.com/sign-up"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Sign Up
-            </a>
-            <a className="header-link" href="#contact">
-              Contact
-            </a>
-          </div>
-        </header>
-        <article className="introduction-container">
-          <h2 className="introduction-header">
-            Find the news that matters to you.
-          </h2>
-          <p className="introduction-text">
-            Easily share anything across the web and connect with those who
-            share your passions.
-          </p>
-          <button className="introduction-button" onClick={this.handleClick}>
-            Create your free account
-          </button>
-        </article>
-        <footer className="home-intro-footer">
-          <a
-            className="more-info-container"
-            onClick={this.handleScroll}
-            href="#about"
-          >
-            <p className="arrow-text">More Info</p>
-            <img className="info-image" src={downArrow} alt="more info" />
-          </a>
-        </footer>
-      </section>
+          </footer>
+        </section>
+      </ScrollableAnchor>
     );
   }
 }
